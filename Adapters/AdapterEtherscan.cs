52f32c46f55ed5900399208744bc4550207f9494
TenNM <pacs3131@gmail.com>
2020-12-19 18:52:30 +0300

    - code cleaning

--- Adapters/AdapterEtherscan.cs
+++ Adapters/AdapterEtherscan.cs
@@ -1,11 +1,6 @@
 ﻿using EtherscanApi.Net.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace isCourseWork
 {
